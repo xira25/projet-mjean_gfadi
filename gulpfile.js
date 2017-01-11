f87da0b47dc501771a8659171f3869fcd4d7b4fe
FadiGarana <fadi.garana@he-arc.ch>
2017-01-11 22:30:28 +0100

    editing deploy function to include index.css

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ function reloadBrowserSync(cb) {
 
 var deploy = require('gulp-deploy-git');
 gulp.task('deploy', function () {
-  return gulp.src('**/*/*', { read: false, cwd: 'dist' })
+  return gulp.src('**/*', { read: false, cwd: 'dist' })
     .pipe(deploy({
       repository: 'git@github.com:heg-web/projet-mjean_gfadi.git',
       remoteBranch: 'gh-pages'
@@ -41,7 +41,7 @@ function watch(done) {
 
   gulp.watch(conf.path.src('app/**/*.html'), gulp.series('partials', reloadBrowserSync));
   gulp.watch([
-    conf.path.src('**/*.css')
+    conf.path.src('*.css')
   ], gulp.series('styles'));
   gulp.watch(conf.path.src('**/*.js'), gulp.series('inject'));
   done();
